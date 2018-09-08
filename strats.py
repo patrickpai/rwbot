d4ce0eae3daaa65c4d56301e2707124bcb5762e3
Patrick Pai <patrick.m.pai@gmail.com>
2018-09-08 16:06:56 -0400

    float

--- strats.py
+++ strats.py
@@ -164,7 +164,7 @@ def etf(xlk, bond, aapl, msft, goog, order_id, num_xlk):
 
 		# print("Parsed prices")
 
-		xlk_fair_buy = ((3 * bond_buy_price) + (2 * aapl_buy_price) + (3 * msft_buy_price) + (2 * goog_buy_price))/10
+		xlk_fair_buy = ((3 * bond_buy_price) + (2 * aapl_buy_price) + (3 * msft_buy_price) + (2 * goog_buy_price))/10.0
 
 		# print("Fair buy: ", xlk_fair_buy)
 		# print("Sell price: ", xlk_sell_price)
@@ -181,7 +181,7 @@ def etf(xlk, bond, aapl, msft, goog, order_id, num_xlk):
 			toReturn.append({"type": "add", "order_id": order_id, "symbol": "MSFT", "dir": "SELL", "price": msft_buy_price, "size": xlk_sell_size*3})
 			toReturn.append({"type": "add", "order_id": order_id, "symbol": "GOOG", "dir": "SELL", "price": goog_buy_price, "size": xlk_sell_size*2})
 
-		xlk_fair_sell = ((3 * bond_sell_price) + (2 * aapl_sell_price) + (3 * msft_sell_price) + (2 * goog_sell_price))/10
+		xlk_fair_sell = ((3 * bond_sell_price) + (2 * aapl_sell_price) + (3 * msft_sell_price) + (2 * goog_sell_price))/10.0
 
 		# print("Fair sell: ", xlk_fair_sell)
 		# print("Buy price: ", xlk_buy_price)
