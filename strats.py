87a312eda106b4f0137b8016a0783ce678c45cc3
theedwinyu <theedwinyu888@gmail.com>
2018-09-08 12:51:11 -0400

    EY: added bond aggro

--- strats.py
+++ strats.py
@@ -16,16 +16,41 @@ def bond_passive(prices, order_id):
 		buy_bond_price = prices["buy"][0][0]
 		buy_size = prices["buy"][0][1]
 
+		if buy_bond_price > 1000:
+			return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": buy_bond_price, "size": buy_size}
+
 	if len(prices["sell"]) > 0:
 		sell_bond_price = prices["sell"][0][0]
 		sell_size = prices["sell"][0][1]
 
-	if buy_bond_price > 1000:
-		return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": buy_bond_price, "size": buy_size}
+		if sell_bond_price < 1000:
+			return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": sell_bond_price, "size": sell_size}
 
-	if sell_bond_price < 1000:
-		return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": sell_bond_price, "size": sell_size}
+def bond_aggro(prices, order_id):
 
+	toReturn = []
+
+	buy_bond_price = 1000
+	buy_size = -1
+
+	sell_bond_price = 1000
+	sell_size = -1
+	
+	if len(prices["buy"]) > 0:
+		buy_bond_price = prices["buy"][0][0]
+		buy_size = prices["buy"][0][1]
+
+		if buy_bond_price < 999:
+
+			return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": buy_bond_price+1, "size": buy_size}
+
+	if len(prices["sell"]) > 0:
+		sell_bond_price = prices["sell"][0][0]
+		sell_size = prices["sell"][0][1]
+
+		if sell_bond_price > 1001:
+
+			return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": sell_bond_price-1, "size": sell_size}
 
 def adr(babz_prices, baba_prices, order_id):
 
