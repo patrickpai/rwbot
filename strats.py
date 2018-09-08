2ead4fece8276604cd7ecd9dd374b645c7c7e131
theedwinyu <theedwinyu888@gmail.com>
2018-09-08 11:11:43 -0400

    EY: created bond strat

---
+++ strats.py
@@ -0,0 +1,21 @@
+import json
+
+def bond_passive(prices, order_id):
+	
+	buy_bond_price = prices["buy"][0][0]
+	buy_size = prices["buy"][0][1]
+
+	sell_bond_price = prices["sell"][0][0]
+	sell_size = prices["sell"][0][1]
+
+	if bond_price > 1000:
+		return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": buy_bond_price, "size": buy_size}
+
+	if bond_price < 1000:
+
+		return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": sell_bond_price, "size": sell_size}
+
+
+
+
+
