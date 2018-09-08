f618d61c3a6bf5d4dffe2c81c16cffd17efe9261
theedwinyu <theedwinyu888@gmail.com>
2018-09-08 11:19:22 -0400

    EY: fixed bond

--- strats.py
+++ strats.py
@@ -2,11 +2,17 @@ import json
 
 def bond_passive(prices, order_id):
 	
-	buy_bond_price = prices["buy"][0][0]
-	buy_size = prices["buy"][0][1]
+	if len(prices["buy"]) >= 0:
+		buy_bond_price = prices["buy"][0][0]
 
-	sell_bond_price = prices["sell"][0][0]
-	sell_size = prices["sell"][0][1]
+	if len(prices["buy"]) > 0:
+		buy_size = prices["buy"][0][1]
+
+	if len(prices["sell"]) >= 0:
+		sell_bond_price = prices["sell"][0][0]
+
+	if len(prices["sell"]) > 0:
+		sell_size = prices["sell"][0][1]
 
 	if bond_price > 1000:
 		return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": buy_bond_price, "size": buy_size}
